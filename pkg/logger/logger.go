32b1f7d9224ce63a7bf09ce377f2349011ecda56
Dmytro Tsapko <dmytro.tsapko@gmail.com>
2018-08-15 22:54:13 +0300

    logger interface was added

---
+++ pkg/logger/logger.go
@@ -0,0 +1,48 @@
+package logger
+
+// Fields represents a set of log message fields
+type Fields map[string]interface{}
+
+type Formatter string
+
+const (
+	TextFormatter Formatter = "TXT"
+	JSONFormatter Formatter = "JSON"
+)
+
+
+type LogEntry interface {
+	Debug(v ...interface{})
+	Debugf(format string, v ...interface{})
+	Debugln(v ...interface{})
+	Info(v ...interface{})
+	Infof(format string, v ...interface{})
+	Infoln(v ...interface{})
+	Warn(v ...interface{})
+	Warnf(format string, v ...interface{})
+	Warnln(v ...interface{})
+	Error(v ...interface{})
+	Errorf(format string, v ...interface{})
+	Errorln(v ...interface{})
+	Fatal(v ...interface{})
+	Fatalf(format string, v ...interface{})
+	Fatalln(v ...interface{})
+	Panic(v ...interface{})
+	Panicf(format string, v ...interface{})
+	Panicln(v ...interface{})
+	WithField(key string, value interface{}) LogEntry
+	WithFields(Fields) LogEntry
+	WithError(err error) LogEntry
+}
+
+type Logger interface {
+	SetLevel(string)
+	SetFormatter(Formatter)
+	SetOutput(string)
+	SetTimestampFormat(format string)
+
+	WithField(key string, value interface{}) LogEntry
+	WithFields(fields Fields) LogEntry
+	WithError(err error) LogEntry
+}
+
